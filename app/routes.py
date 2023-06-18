850fd96a9a528af98683d7c7cafdb60fea366113
uky-syauki <achmad.uky@gmail.com>
2023-06-19 00:08:06 +0800

    Update1.5

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,19 @@
 from flask import render_template, url_for
 from app import app
 from app.form import formDate
+from app.dbHandle import DataBase
+
+DB = DataBase()
 
 @app.route('/')
 @app.route('/index')
 def index():
+	x = DB.forStatYearKey
+	y = DB.forStatYearValue
 	form = formDate()
 	if form.validate_on_submit():
 		pilihan = form.date.data
-		
-	return render_template("index.html", title="Home", user="Ahmad Syauki")
+	return render_template("index.html", title="Home", user="Ahmad Syauki",x=x,y=y)
 
 
 @app.route('/index2')
