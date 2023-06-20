e6dcb43858cc84619a54bba670428497110a2933
uky-syauki <achmad.uky@gmail.com>
2023-06-20 23:17:23 +0800

    Update1.5

--- app/form.py
+++ app/form.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, DateField
+from wtforms import StringField, IntegerField, DateField, BooleanField, SubmitField, RadioField
 from wtforms.validators import InputRequired
 
 class formDate(FlaskForm):
-    date = DateField('Select a Date', validators=[InputRequired()], format="%Y-%m-%d")
+    date = DateField('Tanggal', validators=[InputRequired()], format="%Y-%m-%d")
+    radioTahun = RadioField("Berdasarkan",choices=[(True,'Tahun')])
+    radioTahun = RadioField("Berdasarkan",choices=[(True,"Bulan")])
+    submit = SubmitField("Tampilkan")
     
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect
 from app import app
 from app.form import formDate
 from app.dbHandle import DataBase
@@ -11,8 +11,15 @@ def index():
 	y = allData.cariUntukTahun("2023")
 	form = formDate()
 	if form.validate_on_submit():
-		pilihan = form.date.data
-	return render_template("index.html", title="Home", user="Ahmad Syauki",x=x,y=y)
+		if form.tahun.data == True:
+			tahun = form.date.data.split('-')
+			x = allData.bulan
+			y = allData.cariUntukTahun(tahun[0])
+		if form.bulan.data == True:
+			bulan = form.date.data.split('-')
+			x = allData.tanggal
+			y = allData.cariUntukBulan(bulan[0]+"-"+bulan[1])
+	return render_template("index.html", title="Home", user="Ahmad Syauki",x=x,y=y,form=form)
 
 
 @app.route('/index2')
