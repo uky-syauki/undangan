34af2713657ae96e2db39fc49679605281c9d196
uky-syauki <achmad.uky@gmail.com>
2023-06-22 05:22:01 +0800

    Update1.5

--- app/autoInsertDb.py
+++ app/autoInsertDb.py
@@ -1,6 +1,6 @@
 from random import randint
-from datetime import datetime
 import calendar
+from datetime import datetime
 
 class buat_data:
     def __init__(self):
@@ -8,6 +8,7 @@ class buat_data:
     def jumlah_hari_pada_bulan(self,tahun,bulan):
         return calendar.monthrange(tahun,bulan)[1]
     def set(self,n):
+        n = int(n)
         if n < 10:
             return f"0{n}"
         return n
@@ -17,21 +18,22 @@ class buat_data:
         hasil = []
         ms = datetime.now().strftime("%f")
         for i in range(jumlah):
-            tanggal = set(randint(1,self.jumlah_hari_pada_bulan(int(th),int(bln))))
-            jam = set(randint(0,23))
-            menit = set(randint(0,59))
-            detik = set(randint(0,59))
+            # tanggal = set(randint(1,self.jumlah_hari_pada_bulan(int(th),int(bln))))
+            tanggal = randint(1,20)
+            jam = randint(0,23)
+            menit = randint(0,59)
+            detik = randint(0,59)
             getRand = f"{th}-{self.set(bln)}-{tanggal} {jam}:{menit}:{detik}.{ms}"
             hasil.append(self.toDate(getRand))
         return hasil
     def untuk_tahun(self,th,jumlah):
         hasil = []
         for i in range(jumlah):
-            bulan = set(randint(1,12))
-            tanggal = set(randint(1,self.jumlah_hari_pada_bulan(int(th),int(bulan))))
-            jam = set(randint(0,24))
-            menit = set(randint(0,59))
-            detik = set(randint(0,59))
+            bulan = randint(1,12)
+            tanggal = randint(1,self.jumlah_hari_pada_bulan(int(th),int(bulan)))
+            jam = randint(0,24)
+            menit = randint(0,59)
+            detik = randint(0,59)
             ms = datetime.now().strftime("%f")
             getRand = f"{th}-{bulan}-{tanggal} {jam}:{menit}:{detik}.{ms}"
             hasil.append(self.toDate(getRand))
