d7ff76875625b7af217f3447e1dd37f819d67ce1
uky-syauki <achmad.uky@gmail.com>
2023-06-22 05:07:27 +0800

    Update1.5

--- app/autoInsertDb.py
+++ app/autoInsertDb.py
@@ -1,9 +1,12 @@
 from random import randint
 from datetime import datetime
+import calendar
 
 class buat_data:
     def __init__(self):
         self.format = "%Y-%m-%d %H:%M:%S.%f"
+    def jumlah_hari_pada_bulan(self,tahun,bulan):
+        return calendar.monthrange(tahun,bulan)[1]
     def set(self,n):
         if n < 10:
             return f"0{n}"
@@ -14,7 +17,7 @@ class buat_data:
         hasil = []
         ms = datetime.now().strftime("%f")
         for i in range(jumlah):
-            tanggal = set(randint(1,31))
+            tanggal = set(randint(1,self.jumlah_hari_pada_bulan(int(th),int(bln))))
             jam = set(randint(0,23))
             menit = set(randint(0,59))
             detik = set(randint(0,59))
@@ -25,7 +28,7 @@ class buat_data:
         hasil = []
         for i in range(jumlah):
             bulan = set(randint(1,12))
-            tanggal = set(randint(1,31))
+            tanggal = set(randint(1,self.jumlah_hari_pada_bulan(int(th),int(bulan))))
             jam = set(randint(0,24))
             menit = set(randint(0,59))
             detik = set(randint(0,59))
@@ -33,3 +36,16 @@ class buat_data:
             getRand = f"{th}-{bulan}-{tanggal} {jam}:{menit}:{detik}.{ms}"
             hasil.append(self.toDate(getRand))
         return hasil
+
+buat = buat_data()
+bulan = buat.untuk_bulan(2023,4,5)
+print("isi bualan:")
+for i in bulan:
+    print(i, end=", type: ")
+    print(type(i))
+
+tahun = buat.untuk_tahun(2020,5)
+print("\n\nisi tahun:")
+for i in tahun:
+    print(i, end=", type: ")
+    print(type(i))
